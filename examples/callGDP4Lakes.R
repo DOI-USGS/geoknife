8aed9b94dfee28378e90976eb2d7ce98500b6c28
Jordan S Read <jread@usgs.gov>
2013-06-04 20:49:54 -0500

    set WFS, set WPS for internal

--- examples/callGDP4Lakes.R
+++ examples/callGDP4Lakes.R
@@ -2,6 +2,7 @@ library("rGDP")
 
 # ---- variables -----
 WPS	<-	'http://cida-wiwsc-gdp1qa.er.usgs.gov:8080/gdp-process-wps/WebProcessingService'
+WFS <-  'https://www.sciencebase.gov/catalogMaps/mapping/ows/5064a227e4b0050306263069'
 feature_collection	<-	"sb:mendota"
 attribute	<-	'ComID'
 datasetURI	<-	'dods://cida.usgs.gov/thredds/dodsC/prism'
@@ -13,7 +14,8 @@ rGDP	<-	setAlgorithm(rGDP,getAlgorithms(rGDP)[4])
 
 print(rGDP)
 
-rGDP	<-	setWPS(rGDP,WFS)
+rGDP	<-	setWFS(rGDP,WFS)
+rGDP  <-	setWPS(rGDP,WPS)
 
 getShapefiles(rGDP)
 getAttributes(rGDP,feature_collection)
