6f0f89e37ca467930d3d038257ce97c7b6b91e1d
Jordan S Read <jread@usgs.gov>
2013-08-30 10:44:36 -0500

    example for subset

--- examples/callGDP4Lakes.R
+++ examples/callGDP4Lakes.R
@@ -5,8 +5,9 @@ WPS	<-	'http://cida-wiwsc-gdp1qa.er.usgs.gov:8080/gdp-process-wps/WebProcessingS
 WFS <-  'https://www.sciencebase.gov/catalogMaps/mapping/ows/5064a227e4b0050306263069'
 feature_collection	<-	"sb:mendota"
 attribute	<-	'ComID'
-datasetURI	<-	'http://cida.usgs.gov/thredds/dodsC/prism'
-var	<-	'ppt'
+value <-  '143249470'
+datasetURI  <-	'dods://cida-eros-thredds1.er.usgs.gov:8081/qa/thredds/dodsC/temp/Simard_Pinto_3DGlobalVeg_JGR.tif'
+var	<-	'I0B0'
 # ---- variables -----
 
 rGDP	<-	rGDP()
@@ -15,15 +16,15 @@ rGDP	<-	setAlgorithm(rGDP,getAlgorithms(rGDP)[4])
 print(rGDP)
 
 rGDP	<-	setWFS(rGDP,WFS)
-#rGDP  <-	setWPS(rGDP,WPS)
+#rGDP@WFS_DEFAULT_VERSION = '1.0.0'
+rGDP  <-	setWPS(rGDP,WPS)
 
 getShapefiles(rGDP)
 getAttributes(rGDP,feature_collection)
+getValues(rGDP,feature_collection,attribute)
 rGDP	<-	setFeature(rGDP,list('FEATURE_COLLECTION'=feature_collection,
 	'ATTRIBUTE'=attribute))
 rGDP	<-	setPostInputs(rGDP,list('DATASET_ID'=var,
-                                'TIME_START'="2000-01-01T00:00:00Z",
-                                'TIME_END'="2000-03-01T00:00:00Z",
 								'DATASET_URI'=datasetURI))
 
 print(rGDP)
