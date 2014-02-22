4a82a3eca1d073f72ce0ed59bf810596a2a3783b
Jordan S Read <jread@usgs.gov>
2014-02-21 20:25:26 -0600

    removed prints

--- R/GDP.R
+++ R/GDP.R
@@ -119,7 +119,8 @@ setMethod(f="initialize",signature="rGDP",
 		
 		return(.Object)
 	})
-	setMethod(f = "show",signature = "rGDP",definition = function(object){print(object)})
+
+
 
 #'getAlgorithms
 #'
@@ -731,32 +732,4 @@ setMethod(f = "checkProcess",signature = "rGDP",definition = function(.Object){
 	return(process)
 })
 
-# create summary method......
 
-setMethod(f = "print",signature = "rGDP",
-	function(x,...){
-		cat("*** Class rGDP, method Print *** \n")
-		cat("* WFS_URL:\t");cat(x@WFS_URL,"\n")
-		cat("* WPS_URL:\t");cat(x@WPS_URL,"\n")
-		cat("* algorithm:\t");cat(names(x@algorithm),"\n")
-		cat("* ------postInputs------\n")
-		PI	<-	x@postInputs
-		PI[is.na(PI)] = '[optional]'
-		nms	<-	names(PI)		
-		for (i in 1:length(nms)){cat("\t-",nms[i]);cat(":",PI[[i]],"\n")}
-		cat("* ------feature------\n")
-		PI	<-	x@feature
-		nms	<-	names(PI)
-		PI[is.na(PI)] = '[optional]'
-		for (i in 1:length(nms)){
-			# will skip "hidden"
-			if (!is.null(PI[[i]]) && PI[[i]]!='hidden'){
-				cat("\t-",nms[i]);cat(":",PI[[i]],"\n")
-			} else if (is.null(PI[[i]])){
-				cat("\t-",nms[i]);cat(":",PI[[i]],"\n")
-			}
-		}
-		cat("* processID:\t");cat(x@processID,"\n")
-		cat("**** End Print (rGDP)**** \n")
-	}
-)
