a8873e4a2cf0a25a489dae42d1d399f154ddf6db
Jordan S Read <jread@usgs.gov>
2014-02-22 14:01:35 -0600

    setFeature pulled out and doc

--- R/GDP.R
+++ R/GDP.R
@@ -155,17 +155,6 @@ setGeneric(name="setWFS",def=function(.Object,wfs){standardGeneric("setWFS")})
 #'@keywords setWPS
 #'@export
 setGeneric(name="setWPS",def=function(.Object,wps){standardGeneric("setWPS")})
-#'setFeature
-#'
-#'method for setting the feature elements of the \code{rGDP} object. 
-#'
-#'@param An \code{rGDP} object.
-#'@param a list containing a valid feature collection, or a list of a subset of a valid feature collection.
-#'@return An \code{rGDP} object.
-#'@docType methods
-#'@keywords setFeature
-#'@export
-setGeneric(name="setFeature",def=function(.Object,feature){standardGeneric("setFeature")})
 #'setAlgorithm
 #'
 #'method for setting the process algorithm of the \code{rGDP} object.
@@ -239,36 +228,7 @@ setMethod(f = "setWPS",signature="rGDP",
 		return(.Object)
 	})
 
-# '@rdname setFeature-methods
-# '@aliases setFeature,rGDP-method	
-setMethod(f = "setFeature",signature = "rGDP",
-	definition = function(.Object,feature){
-		
-		if ("LinearRing" %in% names(feature)){
-			# if we are setting the LinearRing, all other feature elements should be wiped
-			if (length(names(feature)) > 1){
-				stop('Cannot set LinearRing and WFS components for single feature')
-			} else {
-				sw.idx	<-	names(.Object@feature)!='LinearRing'
-				hid.feature	<-	.Object@feature[sw.idx]
-				hid.feature[]	<-	'hidden'
-				# set all other elements to 'hidden'
-				.Object@feature	<-	setList(.Object@feature,hid.feature)
-				.Object@feature	<-	setList(.Object@feature,feature)
 
-			}
-		} else {
-			hid.feature	<-	list(LinearRing='hidden')
-			.Object@feature	<-	setList(.Object@feature,feature)
-			.Object@feature	<-	setList(.Object@feature,hid.feature)
-			if ("FEATURE_ATTRIBUTE_NAME" %in% names(.Object@processInputs)){
-			.Object@processInputs	<-	setList(.Object@processInputs,
-				list("FEATURE_ATTRIBUTE_NAME"=.Object@feature$ATTRIBUTE))}
-		}
-	
-		
-		return(.Object)
-	})
 # '@rdname setAlgorithm-methods
 # '@aliases setAlgorithm,rGDP-method	
 setMethod(f = "setAlgorithm",signature = "rGDP",
