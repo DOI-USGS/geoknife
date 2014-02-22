921d195cb02eb4dcd00e9ceaf5eb2be53db784af
Jordan S Read <jread@usgs.gov>
2014-02-21 21:08:20 -0600

    method cleaning and commenting

--- R/initializePostInputs.R
+++ R/initializePostInputs.R
@@ -10,22 +10,23 @@ setMethod(f = "initializePostInputs",signature="rGDP",
 		algorithm	<-	.Object@algorithm
 		processURL	<-	paste(c(.Object@WPS_URL,'?service=WPS&version=',
 			.Object@WPS_DEFAULT_VERSION,'&request=DescribeProcess',
-			'&identifier=',algorithm),collapse="")
-			
+			'&identifier=',algorithm),collapse="")	
 		# test connection with processURL here, provide error if necessary
 		doc	<-	htmlParse(processURL,isURL=TRUE, useInternalNodes = TRUE)
 		optionNd	<-	getNodeSet(doc,'//datainputs/input[@minoccurs=0]/following-sibling::node()[1]')
 		optionLs	<-	vector("list",length(optionNd))
-		optionLs[]	<-	NA
+		optionLs[]	<-	NA # "NA" is the equivalent of "optional" as an input
 		names(optionLs)	<-	sapply(optionNd,xmlValue)
 		
 		requirNd	<-	getNodeSet(doc,'//datainputs/input[@minoccurs>0]/following-sibling::node()[1]')
-		requirLs	<-	vector("list",length(requirNd))
+		requirLs	<-	vector("list",length(requirNd))	# max > 0 is required
 		names(requirLs)	<-	sapply(requirNd,xmlValue)
 		
-		.Object@postInputs	<-	append(optionLs,requirLs)
-		.Object	<-	setPostInputs(.Object,requirLs)
 		
+		#.Object@postInputs	<-	append(optionLs,requirLs)
+		.Object	<-	setPostInputs(.Object,requirLs)
+		.Object	<-	setPostInputs(.Object,optionLs)
+		print(.Object@postInputs)
 		# now set any defaults to their default values
 		defaultNd	<-	getNodeSet(doc,'//datainputs/literaldata/defaultvalue/parent::node()[1]/defaultvalue')
 		defaultLs	<-	vector("list",length(sapply(defaultNd,xmlValue)))
@@ -43,7 +44,9 @@ setMethod(f = "initializePostInputs",signature="rGDP",
 		allowLs[]	<-	sapply(allowNd,xmlValue)
 		names(allowLs)	<-	sapply(getNodeSet(doc,'//datainputs/literaldata/allowedvalues/
 			parent::node()[1]/preceding-sibling::node()[3]'),xmlValue)
-		if (length(allowLs)>0){.Object@postInputs	<-	setList(.Object@postInputs,allowLs)}
+		if (length(allowLs)>0){
+			.Object@postInputs	<-	setList(.Object@postInputs,allowLs)
+		}
 		
 		.Object@postInputs$FEATURE_COLLECTION	<-	NULL # handled elsewhere
 		return(.Object)
