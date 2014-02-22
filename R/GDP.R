e0594684159f8769e8e166e0037d82232ac78988
Jordan S Read <jread@usgs.gov>
2014-02-21 20:33:37 -0600

    pulling out initializePost method

--- R/GDP.R
+++ R/GDP.R
@@ -252,50 +252,9 @@ setGeneric(name="setFeature",def=function(.Object,feature){standardGeneric("setF
 #'@export
 setGeneric(name="setAlgorithm",def=function(.Object,algorithm){standardGeneric("setAlgorithm")})
 
-# move this//?
-setGeneric(name="initializePostInputs",def=function(.Object){standardGeneric("initializePostInputs")})
+
 setGeneric(name="setProcessID",def=function(.Object,processID){standardGeneric("setProcessID")})
 
-setMethod(f = "initializePostInputs",signature="rGDP",
-	definition =	function(.Object){
-		algorithm	<-	.Object@algorithm
-		processURL	<-	paste(c(.Object@WPS_URL,'?service=WPS&version=',
-			.Object@WPS_DEFAULT_VERSION,'&request=DescribeProcess',
-			'&identifier=',algorithm),collapse="")
-		doc	<-	htmlParse(processURL,isURL=TRUE, useInternalNodes = TRUE)
-		optionNd	<-	getNodeSet(doc,'//datainputs/input[@minoccurs=0]/following-sibling::node()[1]')
-		optionLs	<-	vector("list",length(optionNd))
-		optionLs[]	<-	NA
-		names(optionLs)	<-	sapply(optionNd,xmlValue)
-		
-		requirNd	<-	getNodeSet(doc,'//datainputs/input[@minoccurs>0]/following-sibling::node()[1]')
-		requirLs	<-	vector("list",length(requirNd))
-		names(requirLs)	<-	sapply(requirNd,xmlValue)
-		
-		.Object@postInputs	<-	append(optionLs,requirLs)
-		.Object	<-	setPostInputs(.Object,requirLs)
-		
-		# now set any defaults
-		defaultNd	<-	getNodeSet(doc,'//datainputs/literaldata/defaultvalue/parent::node()[1]/defaultvalue')
-		defaultLs	<-	vector("list",length(sapply(defaultNd,xmlValue)))
-		defaultLs[]	<-	sapply(defaultNd,xmlValue)
-		names(defaultLs)	<-	sapply(getNodeSet(doc,'//datainputs/literaldata/defaultvalue/
-			parent::node()[1]/preceding-sibling::node()[3]'),xmlValue)
-			
-		if (length(defaultLs)>0){.Object@postInputs	<-	setList(.Object@postInputs,defaultLs)}
-		
-		# now set any accepted values
-		allowNd    <-	getNodeSet(doc,'//datainputs/literaldata//parent::node()/allowedvalues/value[1]')
-		allowLs	<-	vector("list",length(sapply(allowNd,xmlValue)))
-		allowLs[]	<-	sapply(allowNd,xmlValue)
-		names(allowLs)	<-	sapply(getNodeSet(doc,'//datainputs/literaldata/allowedvalues/
-			parent::node()[1]/preceding-sibling::node()[3]'),xmlValue)
-		if (length(allowLs)>0){.Object@postInputs	<-	setList(.Object@postInputs,allowLs)}
-		
-		.Object@postInputs$FEATURE_COLLECTION	<-	NULL # handled elsewhere
-		return(.Object)
-		
-	})
 
 setMethod(f = "setProcessID",signature="rGDP",
 	definition = function(.Object,processID){
