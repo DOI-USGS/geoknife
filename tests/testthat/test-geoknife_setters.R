be1091fd935247e4695bb80f810f6d49bd7a73a5
Jordan S Read <jread@usgs.gov>
2014-06-11 16:21:14 -0500

    tests for bad list pass to algs

--- tests/testthat/test-geoknife_setters.R
+++ tests/testthat/test-geoknife_setters.R
@@ -4,10 +4,12 @@ gk <- geoknife()
 algs <- getAlgorithms(gk)
 
 test_that("geoknife can set algorithms", {
-	set.bad <- function(gk){
-		setAlgorithm(gk)<-'bad.char'
+	set.bad <- function(gk,val){
+		setAlgorithm(gk)<-val
 	}
-	expect_error(set.bad(gk))
+	expect_error(set.bad(gk,val='bad.char'))
+	expect_error(set.bad(gk,val=NULL))
+	expect_error(set.bad(gk,val=list('junk'='will break process')))
 
 })
 #test_that("geoknife can set process inputs", {
