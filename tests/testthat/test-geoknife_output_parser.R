a0f5d8ad2c7c817a628cb8b49d5667630893d988
Jordan S Read <jread@usgs.gov>
2015-02-08 12:04:41 -0600

    more complex files

--- tests/testthat/test-geoknife_output_parser.R
+++ tests/testthat/test-geoknife_output_parser.R
@@ -3,19 +3,35 @@ context("Test file parsing")
 test_that("timeseries parser works on multi feature, single variable", {
   local_file <- system.file('extdata','tsv_multi_feature.tsv', package = 'geoknife')
   output <- parseTimeseries(local_file, delim = '\t')
-  expect_is(output, "list")
+  expect_is(output, "data.frame")
 })
 
 test_that("timeseries parser works on linear ring, single variable", {
   local_file <- system.file('extdata','tsv_linear_ring.tsv', package = 'geoknife')
   output <- parseTimeseries(local_file, delim = '\t')
-  expect_is(output, "list")
+  expect_is(output, "data.frame")
 })
 
 test_that("timeseries parser works on linear ring, comma delimited", {
   local_file <- system.file('extdata','csv_linear_ring.csv', package = 'geoknife')
   output <- parseTimeseries(local_file, delim = ',')
-  expect_is(output, "list")
+  expect_is(output, "data.frame")
   expect_error(parseTimeseries(local_file, delim = '\t'))
 })
+
+test_that("timeseries parser works on complex output tsv", {
+  local_file <- system.file('extdata','tsv_multi_var_feature_stat.tsv', package = 'geoknife')
+  output <- parseTimeseries(local_file, delim = '\t')
+  expect_is(output, "data.frame")
+  expect_error(parseTimeseries(local_file, delim = ','))
+})
+
+test_that("timeseries parser works on complex output csv", {
+  local_file <- system.file('extdata','csv_multi_feature_stat.csv', package = 'geoknife')
+  output <- parseTimeseries(local_file, delim = ',')
+  expect_is(output, "data.frame")
+  expect_error(parseTimeseries(local_file, delim = ' '))
+})
+
+
 
