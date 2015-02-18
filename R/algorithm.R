404b15845b993a3b46f78cd56623bac57d35a8f0
Jordan S Read <jread@usgs.gov>
2015-02-18 16:40:11 -0600

    working on new model for geoknife classes

--- R/algorithm.R
+++ R/algorithm.R
@@ -1,61 +1,60 @@
-#'@title set processing algorithm
-#'
-#'@details method for setting the process algorithm of the \code{geoknife} object.
-#'
-#'@param .Object a \code{geoknife} object.
-#'@param value a list for a valid algorithm, including values for name & location
-#'@return An \code{geoknife} object.
-#'@docType methods
-#'@keywords methods
-#'@examples 
-#'gk <- geoknife() # create geoknife object
-#'algorithm <- list("Area Grid Statistics (weighted)"=
-#'"gov.usgs.cida.gdp.wps.algorithm.FeatureWeightedGridStatisticsAlgorithm")
-#'algorithm(gk) <- algorithm
-#'@author Jordan S. Read
-#'@rdname algorithm-set
-#'@export
-setGeneric(name="algorithm<-",def=function(.Object,value){
-  standardGeneric("algorithm<-")
-})
-
-
-#'@rdname algorithm-set
-#'@aliases algorithm<-
-setReplaceMethod(f = "algorithm",signature = "geoknife",
-	definition = function(.Object,value){
-		.Object@algorithm	<-	value
-		# now, initialize posts
-		.Object	<-	initializeProcessInputs(.Object)
-		return(.Object)
-	})
-
-#'@title get processing algorithm
-#'
-#'@details method for getting the process algorithm of the \code{geoknife} object.
-#'
-#'@param .Object a \code{geoknife} object.
-#'@return An \code{geoknife} object's algorithm
-#'@keywords methods
-#'@examples 
-#'gk <- geoknife() # create geoknife object
-#'algorithm <- list("Area Grid Statistics (weighted)"=
-#'"gov.usgs.cida.gdp.wps.algorithm.FeatureWeightedGridStatisticsAlgorithm")
-#'algorithm(gk) <- algorithm
-#'algorithm(gk)
-#'@author Jordan S. Read
-#'@rdname algorithm-methods
-#'@name algorithm
-#'@docType methods
-#'@export
-setGeneric(name="algorithm",def=function(.Object){
-  standardGeneric("algorithm")
-})
-
-#'@rdname algorithm-methods
-#'@aliases algorithm,geoknife-method
-setMethod(f = "algorithm",signature="geoknife",
-          definition = function(.Object){
-            return(.Object@algorithm)
-          }
-)
+# #'@title set processing algorithm
+# #'
+# #'@details method for setting the process algorithm of the \code{geoknife} object.
+# #'
+# #'@param .Object a \code{geoknife} object.
+# #'@param value a list for a valid algorithm, including values for name & location
+# #'@return An \code{geoknife} object.
+# #'@docType methods
+# #'@keywords methods
+# #'@examples 
+# #'gk <- geoknife() # create geoknife object
+# #'algorithm <- list("Area Grid Statistics (weighted)"=
+# #'"gov.usgs.cida.gdp.wps.algorithm.FeatureWeightedGridStatisticsAlgorithm")
+# #'algorithm(gk) <- algorithm
+# #'@author Jordan S. Read
+# #'@name algorithm<-
+# #'@rdname algorithm-set
+# #'@export
+# setGeneric(name="algorithm<-",def=function(.Object,value){
+#   standardGeneric("algorithm<-")
+# })
+# 
+# #'@aliases algorithm<-
+# #'@rdname algorithm-set
+# setReplaceMethod(f = "algorithm",signature = "geoknife",
+# 	definition = function(.Object,value){
+# 		.Object@algorithm	<-	value
+# 		# now, initialize posts
+# 		.Object	<-	initializeProcessInputs(.Object)
+# 		return(.Object)
+# 	})
+# 
+# #'@title get processing algorithm
+# #'
+# #'@details method for getting the process algorithm of the \code{geoknife} object.
+# #'
+# #'@param .Object a \code{geoknife} object.
+# #'@return An \code{geoknife} object's algorithm
+# #'@keywords methods
+# #'@examples 
+# #'gk <- geoknife() # create geoknife object
+# #'algorithm <- list("Area Grid Statistics (weighted)"=
+# #'"gov.usgs.cida.gdp.wps.algorithm.FeatureWeightedGridStatisticsAlgorithm")
+# #'algorithm(gk) <- algorithm
+# #'algorithm(gk)
+# #'@author Jordan S. Read
+# #'@rdname algorithm-methods
+# #'@name algorithm
+# #'@docType methods
+# #'@export
+# setGeneric(name="algorithm",def=function(.Object){
+#   standardGeneric("algorithm")
+# })
+# 
+# #'@describeIn algorithm
+# setMethod(f = "algorithm",signature="geoknife",
+#           definition = function(.Object){
+#             return(.Object@algorithm)
+#           }
+# )
---
+++ R/webdata-obj.R
@@ -0,0 +1,93 @@
+
+setClass(
+  Class = "webdata",
+  representation = representation(
+    times = 'POSIXct',
+    url = "character",
+    variables = "character")
+)
+setMethod("initialize", signature = "webdata", 
+          definition = function(.Object, times = as.POSIXct(c(NA,NA)), url = as.character(NA), variables = as.character(NA)){
+            
+            times(.Object) = times
+            .Object@url = url
+            .Object@variables = variables
+            return(.Object)
+          })
+
+#' create webdata object
+#' @description A class representing a web dataset.
+#'
+#' @slot times value of type \code{"POSIXct"}, start and stop dates for data
+#' @slot url value of type \code{"character"}, the web location for the dataset
+#' @slot variable value of type \code{"character"}, the variable(s) for data
+#'
+#' @return the webdata object representing a dataset and parameters
+#' @author Jordan S Read
+#' @rdname webdata-methods
+#' @export
+
+setGeneric("webdata", function(...) {
+  standardGeneric("webdata")
+})
+
+
+#' @rdname webdata-methods
+#' @aliases webdata,webdata-method
+setMethod("webdata", signature(), function(...) {
+  ## create new webdata object
+  webdata <- new("webdata",...)
+  return(webdata)
+})
+
+#'@rdname webdata-methods
+#'@aliases times<-,webdata-method
+#'@author Jordan S. Read
+#'@export
+setGeneric(name="times<-",def=function(.Object, values){standardGeneric("times<-")})
+
+
+setReplaceMethod(f = "times",signature = "webdata",
+                 definition = function(.Object, values){
+                   if (length(values) != 2){
+                     stop('times input must be a POSIXct vector of length 2')
+                   }
+                   .Object@times <- values
+                   return(.Object)
+})
+
+#'@rdname webdata-methods
+#'@aliases times,webdata-method
+#'@author Jordan S. Read
+#'@export
+setGeneric(name="times",def=function(.Object){standardGeneric("times")})
+
+setMethod(f = "times",signature = "webdata",
+                 definition = function(.Object){
+                   return(.Object@times)
+                 })
+
+setMethod("[<-", c("webdata", "character", "POSIXct"),
+          ## we won't support subsetting on j; dispatching on 'drop' doesn't
+          ## make sense (to me), so in rebellion we'll quietly ignore it.
+          function (x, i, j, ..., value) 
+          {
+            x@times[i] <- value
+            ## .Object
+            ## clever: by default initialize is a copy constructor, too
+            #initialize(.Object, times=.Object@times[i])
+            return(x)
+          })
+
+setMethod("[", c("webdata", "integer", "missing", "ANY"),
+          ## we won't support subsetting on j; dispatching on 'drop' doesn't
+          ## make sense (to me), so in rebellion we'll quietly ignore it.
+          function(x, i, j, ..., drop=TRUE)
+          {
+            ## .Object
+            browser()
+            ## clever: by default initialize is a copy constructor, too
+            #initialize(.Object, times=.Object@times[i])
+            initialize(x, times=x@times[i])
+          })
+
