b01badd29bc2988286bc537c82ad8d8f414c96ae
Jordan S Read <jread@usgs.gov>
2015-02-19 15:54:16 -0600

    to algorithms and simplified.

--- R/getAlgorithms.R
+++ R/getAlgorithms.R
@@ -1,4 +1,4 @@
-#'@details getAlgorithms is a method for finding algorithm names and locations for
+#'@details algorithms is a method for finding algorithm names and locations for
 #' a \code{geoknife} object.
 #'
 #'@param .Object a \code{geoknife} object with a valid WPS url.
@@ -6,26 +6,61 @@
 #'@docType methods
 #'@description Get algorithms for \code{geoknife}
 #'@title Get processing algorithms
-#'@keywords getAlgorithms
+#'@keywords methods
 #'@author Jordan S. Read
 #'@seealso \code{setAlgorithm<-}
-#'@examples gk <- geoknife() # create geoknife object
-#'getAlgorithms(gk) # list Geo Data Portal processing algorithms
+#'@examples 
+#'wp <- webprocess() # create geoknife object
+#'algorithms(wp)
+#'algorithms() # list Geo Data Portal processing algorithms
 #'@export
-setGeneric(name="getAlgorithms",def=function(.Object){standardGeneric("getAlgorithms")})
+setGeneric(name="algorithms",def=function(.Object){standardGeneric("algorithms")})
 
-# '@rdname getAlgorithms-methods
-# '@aliases getAlgorithms,geoknife-method	
-setMethod(f = "getAlgorithms",signature="geoknife",
+# '@rdname algorithms-methods
+# '@aliases algorithms,webprocess-method 	
+setMethod(f = "algorithms",signature="webprocess",
 	definition = function(.Object){
-		processURL	<-	paste(c(.Object@WPS_URL,'?service=WPS&version=',
-			.Object@WPS_DEFAULT_VERSION,'&request=GetCapabilities'),collapse="")
-		algorithm.Loc	<-	parseXMLnodes(processURL,"process","identifier",key=NA)
-		algorithm.Nm	<-	parseXMLnodes(processURL,"process","title",key=NA)
-		algorithms	<-	list()
-		for (i in 1:length(algorithm.Nm)){
-			algorithms[[i]]	<-	algorithm.Loc[i]
-		}
-		names(algorithms)	<-	algorithm.Nm
-		return(algorithms)
+		
+	  algorithmParse(wps_url = .Object@wps_url, wps_version = .Object@WPS_VERSION)
 	})
+
+# '@rdname algorithms-methods
+# '@aliases algorithms,webprocess-method  
+setMethod(f = "algorithms",signature="missing",
+          definition = function(.Object){
+            algorithms(webprocess())
+          }
+)
+
+# '@rdname algorithms-methods
+# '@aliases algorithms,geojob-method  
+setMethod(f = "algorithms",signature="geojob",
+          definition = function(.Object){
+            algorithmParse(wps_url = .Object@WPS_URL, wps_version = .Object@WPS_DEFAULT_VERSION)
+          }
+)
+
+setMethod(f = "algorithms",signature="character",
+          definition = function(.Object){
+            algorithmParse(processURL = .Object)
+          }
+)
+
+#'@importFrom httr GET
+algorithmParse = function(wps_url, wps_version, processURL){
+  
+  if (missing(processURL)){
+    processURL  <-  paste(c(wps_url,'?service=WPS&version=',
+                            wps_version,'&request=GetCapabilities'),collapse="")
+  }
+  
+  doc <- GET(processURL)
+  algorithm.Loc	<-	parseXMLnodes(doc,"process","identifier",key=NA)
+  algorithm.Nm	<-	parseXMLnodes(doc,"process","title",key=NA)
+  algorithms	<-	list()
+  for (i in 1:length(algorithm.Nm)){
+    algorithms[[i]]	<-	algorithm.Loc[i]
+  }
+  names(algorithms)	<-	algorithm.Nm
+  return(algorithms)
+}
