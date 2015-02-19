01ad2bc92a9ba4e91c8771fbb220e4314bdfbfa8
Jordan S Read <jread@usgs.gov>
2015-02-19 08:14:04 -0600

    webdata object and methods

--- R/webdata-obj.R
+++ R/webdata-obj.R
@@ -41,8 +41,10 @@ setMethod("webdata", signature(), function(...) {
 })
 
 #'@rdname webdata-methods
+#'@usage
+#'times(webdata()) <- as.POXIXct(c("2012-11-04", "2012-11-12"))
+#'times(webdata())[1] <- as.POXIXct("2012-11-04")
 #'@aliases times<-,webdata-method
-#'@author Jordan S. Read
 #'@export
 setGeneric(name="times<-",def=function(.Object, values){standardGeneric("times<-")})
 
@@ -53,12 +55,15 @@ setReplaceMethod(f = "times",signature = "webdata",
                      stop('times input must be a POSIXct vector of length 2')
                    }
                    .Object@times <- values
+                   
+                   if (values[1] >= values[2]){
+                     stop('time start must proceed time stop in "times" slot for webdata')
+                   }
                    return(.Object)
 })
 
 #'@rdname webdata-methods
 #'@aliases times,webdata-method
-#'@author Jordan S. Read
 #'@export
 setGeneric(name="times",def=function(.Object){standardGeneric("times")})
 
@@ -66,6 +71,57 @@ setMethod(f = "times",signature = "webdata",
                  definition = function(.Object){
                    return(.Object@times)
                  })
+
+
+#'@rdname webdata-methods
+#'@aliases url<-,webdata-method
+#'@export
+setGeneric(name="url<-",def=function(.Object, values){standardGeneric("url<-")})
+
+setMethod(f = "url<-",signature = "webdata",
+          definition = function(.Object, values){
+            if (length(values) != 1){
+              stop('url must be a single character string')
+            }
+            .Object@url <- values
+            return(.Object)
+          })
+
+#'@rdname webdata-methods
+#'@aliases url,webdata-method
+#'@export
+setGeneric(name="url",def=function(.Object){standardGeneric("url")})
+
+setMethod(f = "url",signature = "webdata",
+          definition = function(.Object){
+            return(.Object@url)
+          })
+
+
+#'@rdname webdata-methods
+#'@aliases variables,webdata-method
+#'@export
+setGeneric(name="variables",def=function(.Object){standardGeneric("variables")})
+
+setMethod(f = "variables",signature = "webdata",
+          definition = function(.Object){
+            return(.Object@variables)
+          })
+
+#'@rdname webdata-methods
+#'@aliases variables<-,webdata-method
+#'@export
+setGeneric(name="variables<-",def=function(.Object, values){standardGeneric("variables<-")})
+
+setMethod(f = "variables<-",signature = "webdata",
+          definition = function(.Object, values){
+            .Object@variables <- values
+            return(.Object)
+          })
+
+
+
+
 
 setMethod("[<-", c("webdata", "character", "POSIXct"),
           ## we won't support subsetting on j; dispatching on 'drop' doesn't
