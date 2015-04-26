1b9acf6e40896c2bafa8cd1458cc26db9c8edf0f
Jordan S Read <jread@usgs.gov>
2015-04-26 08:10:58 -0500

    creating get GML IDs when values are set

--- R/values-webgeom.R
+++ R/values-webgeom.R
@@ -18,7 +18,7 @@ setGeneric(name="values",def=function(.Object){standardGeneric("values")})
 #'@rdname values
 setMethod(f = "values<-",signature(.Object = "webgeom"), definition = function(.Object, value){
   .Object@values <- value
-  .Object@GML_IDs <- "CONUS_States.28"
+  .Object@GML_IDs <- fetchGML_IDs(.Object)
   return(.Object)})
 
 #'@aliases values
@@ -29,3 +29,21 @@ setMethod(f = "values",signature="webgeom",
           }
 )
 
+#' @title fetch GML_IDs from WFS
+#' @description fetch GML_IDs from WFS when geom, attribute, and values are specified
+#' @param .Object a webgeom object
+#' @importFrom httr GET content
+#' @keywords internal 
+fetchGML_IDs <- function(.Object){
+  url <- sprintf('%s?service=WFS&version=%s&request=GetFeature&typename=%s&propertyname=%s',
+                 url(.Object), version(.Object), geom(.Object), .Object@attribute)
+  ns_geom <- strsplit(geom(.Object), ":")[[1]][1]
+  response <- GET(url)
+  xml <- content(response)
+  value_path <- sprintf('//gml:featureMembers/%s/%s:%s', geom(.Object), ns_geom, .Object@attribute)
+  value_names <- sapply(getNodeSet(xml,paste0(value_path, '/node()[1]')), 
+                        FUN = function(x) xmlValue(x)[1])
+  match_id <- which(value_names == values(.Object)) # CHECK ORDER!!!
+  gml_id = getNodeSet(xml,paste0(value_path,'/parent::node()[1]/@gml:id'))[[match_id]][['id']]
+  return(gml_id)  
+}
