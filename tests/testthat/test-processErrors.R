5d903087f2af7a467e52a0ff4c4295e7c45df7ba
Jordan S Read <jread@usgs.gov>
2015-05-16 14:23:49 -0500

    test for #124

--- tests/testthat/test-processErrors.R
+++ tests/testthat/test-processErrors.R
@@ -7,7 +7,14 @@ fabric <- webdata('prism', variables = 'wrong')
 
 test_that("java stack failure", {
   
-  expect_equal(geoknife:::getJobState(), 'none')
   job <- geoknife(stencil, fabric, waitUntilFinished = TRUE)
   expect_equal(geoknife:::getJobState(), "ProcessFailed")
+})
+
+context("testing required field as NA in webdata")
+
+cancel()
+test_that("required field as NA in webdata", {
+  variables(fabric) <- as.character(NA)
+  expect_error(geoknife(stencil, fabric))
 })
