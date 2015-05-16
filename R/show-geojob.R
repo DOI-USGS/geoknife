04d11c71ebd4c8b31f61501941f21e94c4609b39
Jordan S Read <jread@usgs.gov>
2015-05-16 13:59:35 -0500

    tiny bug in show

--- R/show-geojob.R
+++ R/show-geojob.R
@@ -1,6 +1,6 @@
 setMethod("show", "geojob", function(object){
   cat('An object of class "geojob":\n')
   cat('url:',url(object),'\n')
-  cat('xml:\n', cat(xml(object)),'\n')
+  cat('xml:\n', xml(object),'\n')
   cat('id:', id(object))
 })
