ebb8eb47f1a2415f8b329ec74fbf9b39cc0edaec
Jordan S Read <jread@usgs.gov>
2015-05-23 07:52:29 -0500

    fix to demo

--- demo/iclus_categorical.R
+++ demo/iclus_categorical.R
@@ -10,7 +10,7 @@ fabric <- webdata(url = ' http://cida.usgs.gov/thredds/dodsC/iclus/hc') #, varia
 
 #see what variables are available:
 query(fabric, 'variables')
-variables(fabric) <- c('housing_classes_iclus_a1_2010', 'housing_classes_iclus_bc_2100')
+variables(fabric) <- c('housing_classes_iclus_a1_2010', 'housing_classes_iclus_a1_2100')
 
 knife <- webprocess(algorithm = list('Categorical Coverage Fraction'="gov.usgs.cida.gdp.wps.algorithm.FeatureCategoricalGridCoverageAlgorithm"))
 job <- geoknife(stencil, fabric, knife, email = 'your.email.here@gardenmail.com')
