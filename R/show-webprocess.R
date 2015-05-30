a3992ce3bf3d1a7b64a5353faafe9f41cae36a9d
Jordan S Read <jread@usgs.gov>
2015-05-30 13:46:34 -0500

    updating show method for webprocess

--- R/show-webprocess.R
+++ R/show-webprocess.R
@@ -1,9 +1,13 @@
 setMethod("show", "webprocess", function(object){
   cat('An object of class "webprocess":\n')
   cat('url:',url(object),'\n')
-  cat('algorithm:', names(algorithm(object)),'>\n')
+  cat('algorithm:', names(algorithm(object)),'\n')
   cat('version:', version(object),'\n')
-  cat('required inputs: {currently in development}', '\n')#object@processInputs
+  cat('process inputs: \n')
+  inputs <- inputs(object)
+  for (i in 1:length(inputs)){
+    cat(paste0('   ',names(inputs)[i],": ",inputs[[i]], '\n'))
+  }
   cat('wait:', object@wait, '\n')
   cat('email:', object@email, '\n' )
 })
