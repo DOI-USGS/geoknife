8583b8504889bc1949eff31d079f6ff2598a2413
Jordan S Read <jread@usgs.gov>
2015-05-31 08:15:25 -0500

    fix to tests

--- tests/testthat/test-webprocess_input.R
+++ tests/testthat/test-webprocess_input.R
@@ -11,18 +11,18 @@ test_that("setting with optional arguments is possible",{
   
   wp <- webprocess(DELIMITER = 'TAB')
   wp2 <- webprocess()
-  expect_true(inputs(wp, 'DELIMITER') !=  inputs(wp2, 'DELIMITER'))
+  expect_true(inputs(wp, 'DELIMITER')[[1]] !=  inputs(wp2, 'DELIMITER')[[1]])
   expect_is(inputs(wp), 'list')
   inputs(wp) <- list(DELIMITER = 'COMMA', SUMMARIZE_FEATURE_ATTRIBUTE = 'false')
-  expect_true(inputs(wp, 'DELIMITER') ==  inputs(wp2, 'DELIMITER'))
+  expect_equal(inputs(wp, 'DELIMITER'),inputs(wp2, 'DELIMITER'))
 })
 
 test_that("get inputs works as expected",{
   
   wp <- webprocess(DELIMITER = 'TAB')
-  expect_is(inputs(wp, "DELIMITER")[[1]], 'TAB')
-  expect_equal(length(inputs(wp, "DELIMITER")) == 1)
-  expect_equal(length(inputs(wp, "DELIMITER", "SUMMARIZE_FEATURE_ATTRIBUTE")) == 2)
+  expect_equal(inputs(wp, "DELIMITER")[[1]], 'TAB')
+  expect_equal(length(inputs(wp, "DELIMITER")), 1)
+  expect_equal(length(inputs(wp, "DELIMITER", "SUMMARIZE_FEATURE_ATTRIBUTE")), 2)
 })
 
 test_that("reseting algorithm sets inputs back to defaults",{
