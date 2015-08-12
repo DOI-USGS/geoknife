19ef9d5daf5ddf106bf8d9569f12a08f9d9558f7
Jordan S Read <jread@usgs.gov>
2015-08-12 08:39:38 -0500

    updating print to be less confusing

--- R/show-webprocess.R
+++ R/show-webprocess.R
@@ -2,7 +2,7 @@ setMethod("show", "webprocess", function(object){
   cat('An object of class "webprocess":\n')
   cat('url:',url(object),'\n')
   cat('algorithm:', names(algorithm(object)),'\n')
-  cat('version:', version(object),'\n')
+  cat('web processing service version:', version(object),'\n')
   cat('process inputs: \n')
   inputs <- inputs(object)
   for (i in 1:length(inputs)){
