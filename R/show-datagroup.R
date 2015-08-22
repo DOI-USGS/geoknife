d8d1242b11181f35e06979258a3d9691884dfc40
Jordan S Read <jread@usgs.gov>
2015-08-21 19:48:12 -0500

    updates per aappling comments

--- R/show-datagroup.R
+++ R/show-datagroup.R
@@ -2,8 +2,8 @@ setMethod("show", "datagroup", function(object){
   cat('An object of class "datagroup":\n')
   for (i in 1:length(object@group)){
     cat(paste0('[',i,'] '))
-    cat(names(object@group)[i],'\n')
-    cat('url:',object@group[[i]],'\n')
+    cat(dt@group[[i]]$title,'\n')
+    cat('  url:',dt@group[[i]]$url,'\n')
   }
   
 })
