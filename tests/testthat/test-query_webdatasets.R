339221b05f01bec0553ffa4dbbf8149c47674303
Jordan S Read <jread@usgs.gov>
2015-08-23 10:14:04 -0500

    the rest of #191 (url())

--- tests/testthat/test-query_webdatasets.R
+++ tests/testthat/test-query_webdatasets.R
@@ -10,3 +10,17 @@ test_that("webdata query returns a list", {
   wd <- webdata(datasets[4])
   expect_is(query(wd,'variables'), 'character')
 })
+
+test_that("webdata title returns a title", {
+  expect_is(title(datasets[4]), 'character')
+})
+
+test_that("webdata url returns a url", {
+  expect_is(url(datasets[4]), 'character')
+  expect_is(url(datasets[4:5]), 'character')
+})
+
+
+test_that("webdata abstract returns an abstract", {
+  expect_is(url(datasets[4]), 'character')
+})
