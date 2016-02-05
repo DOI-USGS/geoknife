dd37ece1b074d29f2486e4c787ba0404fa78db2e
Jordan S Read <jread@usgs.gov>
2016-02-05 16:46:22 -0600

    gconfig things

---
+++ tests/testthat/test-gconfig.R
@@ -0,0 +1,18 @@
+context("gconfig")
+
+
+test_that("can get read.only variables", {
+  expect_equal(gconfig(no.readonly = FALSE), gconfig())
+  expect_false(length(gconfig(no.readonly = TRUE)) == length(gconfig()))
+  expect_is(inputs(wp), 'list')
+})
+
+test_that("can't set read.only variables", {
+  expect_error(gconfig('version'='2.0.0'))
+})
+
+test_that("can set !read.only variables", {
+  expect_false(gconfig('wait'))
+  gconfig('wait'=TRUE)
+  expect_true(gconfig('wait'))
+})
