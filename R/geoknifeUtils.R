91ae5b335cf0fab488aa7f292528a3b1dab787d8
Jordan S Read <jread@usgs.gov>
2016-02-05 20:28:09 -0600

    for #244

--- R/geoknifeUtils.R
+++ R/geoknifeUtils.R
@@ -9,18 +9,26 @@ geoknifeUserAgent <- function() {
   )
   c(`User-Agent`=paste0(names(versions), "/", versions, collapse = " "))
 }
+#'@importFrom httr verbose
+gverbose <- function(){
+  if (gconfig('verbose'))
+    httr::verbose()
+  else 
+    NULL
+}
 
-#'@importFrom httr GET add_headers
+#'@importFrom httr GET add_headers verbose
 gGET <- function(url, ...){
   # I don't like doing this, but the GDP response comes back as `content-type`="text/xml" instead of 
   # `content-type`="text/xml; charset=UTF-8" so we get a charset warning message
-  suppressWarnings(httr::GET(url=url, ..., add_headers(geoknifeUserAgent())))
+  
+  suppressWarnings(httr::GET(url=url, gverbose(), ..., add_headers(geoknifeUserAgent())))
   
 }
 
 #'@importFrom httr POST add_headers
 gPOST <- function(url, config = list(), ...){
-  suppressWarnings(httr::POST(url=url, config=config, ..., add_headers(geoknifeUserAgent())))
+  suppressWarnings(httr::POST(url=url, config=config, gverbose(), ..., add_headers(geoknifeUserAgent())))
 }
 
 #' drop in replacement for httr switching to xml2 from XML
