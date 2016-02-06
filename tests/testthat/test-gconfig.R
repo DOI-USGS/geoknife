9610a3605409ba4582f5f7888692e91673649626
Jordan S Read <jread@usgs.gov>
2016-02-06 07:51:10 -0600

    more gconfig tests

--- tests/testthat/test-gconfig.R
+++ tests/testthat/test-gconfig.R
@@ -8,10 +8,17 @@ test_that("can get read.only variables", {
 
 test_that("can't set read.only variables", {
   expect_error(gconfig('version'='2.0.0'))
+  expect_error(gconfig('version'='2.0.0', 'wait'=TRUE))
 })
 
 test_that("can set !read.only variables", {
   expect_false(gconfig('wait'))
   gconfig('wait'=TRUE)
   expect_true(gconfig('wait'))
+})
+
+test_that("can set !read.only variables on webprocess()", {
+  testthat::skip_on_cran()
+  gconfig('email'='none@gmail.com')
+  expect_equal(webprocess()@email, 'none@gmail.com')
 })
