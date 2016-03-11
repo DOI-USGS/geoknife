0c6595970d6b6f200a2c9c905a931dfa72b03820
Jordan S Read <jread@usgs.gov>
2016-03-11 16:14:38 -0600

    start on #163. Needs tests

--- R/dodsReplace.R
+++ R/dodsReplace.R
@@ -1,10 +1,13 @@
+#' @importFrom httr HEAD headers
 dodsReplace  <-	function(uri){
   # checks for dods or opendap, and replaces
-  if (grepl('dodsC',uri)){
+  
+  if (substr(uri, 1, 4) == 'dods')
+    return(uri)
+  
+  dods.serve <- httr::headers(httr::HEAD(uri))$`xdods-server`
+  if (!is.null(dods.serve)){
     uri	<-	gsub('http', 'dods', uri)
-  }
-  if (grepl('opendap',uri)){
-    uri	<-	gsub('http', 'opendap', uri)
   }
    
   return(uri)
