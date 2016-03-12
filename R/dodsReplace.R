4800f657978e5175f673b9d48c86b2b24ae991fc
Jordan S Read <jread@usgs.gov>
2016-03-12 08:51:39 -0600

    for #163

--- R/dodsReplace.R
+++ R/dodsReplace.R
@@ -6,7 +6,7 @@ dodsReplace  <-	function(uri){
     return(uri)
   
   dods.serve <- httr::headers(httr::HEAD(uri))$`xdods-server`
-  if (!is.null(dods.serve)){
+  if (!is.null(dods.serve) || grepl('dodsC',uri)){ # catch the case where prism, comes back 400 from HEAD, but is dods
     uri	<-	gsub('http', 'dods', uri)
   }
    
