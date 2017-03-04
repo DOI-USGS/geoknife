b497cf37e8699e0c3d84ff189108c0c9a6277b2e
Jordan S Read <jread@usgs.gov>
2017-03-03 18:07:46 -0600

    fixes #215

--- R/successful.R
+++ R/successful.R
@@ -1,56 +1,55 @@
-#'@title Convenience function for GDP process state
-#'@rdname successful-methods
-#'@aliases
-#'successful
-#'running
-#'error
-#'@usage
-#'successful(.Object, retry)
-#'error(.Object, retry)
-#'running(.Object, retry)
+#' @title Convenience function for GDP process state
+#' @rdname successful-methods
+#' @aliases
+#' successful
+#' running
+#' error
+#' @usage
+#' successful(.Object, retry)
+#' error(.Object, retry)
+#' running(.Object, retry)
 #'
-#'@param .Object a \linkS4class{geojob} object
-#'@param retry attempt to retry again if communication failed with the server
-#'@return TRUE/FALSE indicating if process is in the given state (error, processing, successful)
-#'@description Simple wrapper to check process status
+#' @param .Object a \linkS4class{geojob} object
+#' @param retry attempt to retry again if communication failed with the server
+#' @return TRUE/FALSE indicating if process is in the given state (error, processing, successful)
+#' @description Simple wrapper to check process status
 #'
-#'@author Luke Winslow, Jordan S Read
-#'@seealso \code{\link{check}}
+#' @author Luke Winslow, Jordan S Read
+#' @seealso \code{\link{check}}
 #'
-#'@examples
-#'\dontrun{
-#'wp <- quick_wp()
-#'job <- geoknife(stencil = c(-89,42), fabric = 'prism', knife = wp)
-#'check(job)
+#' @examples
+#' \dontrun{
+#' job <- geoknife(stencil = c(-89,42), fabric = 'prism')
+#' check(job)
 #'
-#'running(job)
-#'error(job)
-#'successful(job)
-#'}
+#' running(job)
+#' error(job)
+#' successful(job)
+#' }
 #'
-#'@export
+#' @export
 setGeneric(name="successful",def=function(.Object, retry){standardGeneric("successful")})
 
-#'@rdname successful-methods
-#'@aliases successful
+#' @rdname successful-methods
+#' @aliases successful
 setMethod(f = "successful", signature(.Object = "geojob", retry = "logical"), definition = function(.Object, retry){
   successful(id(.Object), retry = retry)
 })
 
-#'@rdname successful-methods
-#'@aliases successful
+#' @rdname successful-methods
+#' @aliases successful
 setMethod(f = "successful", signature(.Object = "geojob", retry = "missing"), definition = function(.Object, retry){
   successful(id(.Object), retry = FALSE)
 })
 
-#'@rdname successful-methods
-#'@aliases successful
+#' @rdname successful-methods
+#' @aliases successful
 setMethod(f = "successful", signature(.Object = "character", retry = "missing"), definition = function(.Object, retry){
   successful(.Object, retry = FALSE)
 })
 
-#'@rdname successful-methods
-#'@aliases successful
+#' @rdname successful-methods
+#' @aliases successful
 setMethod(f = "successful", signature(.Object = "character", retry = "logical"), definition = function(.Object, retry){
   process = check(.Object)
   if (process$status == 'unknown' && retry){
@@ -61,29 +60,29 @@ setMethod(f = "successful", signature(.Object = "character", retry = "logical"),
   return(process$statusType == "ProcessSucceeded")
 })
 
-#'@export
+#' @export
 setGeneric(name="running", def = function(.Object, retry){standardGeneric("running")})
 
-#'@rdname successful-methods
-#'@aliases running
+#' @rdname successful-methods
+#' @aliases running
 setMethod(f = "running", signature(.Object = "geojob", retry = "logical"), definition = function(.Object, retry){
   running(id(.Object), retry = retry)
 })
 
-#'@rdname successful-methods
-#'@aliases running
+#' @rdname successful-methods
+#' @aliases running
 setMethod(f = "running", signature(.Object = "geojob", retry = "missing"), definition = function(.Object, retry){
   running(id(.Object), retry = FALSE)
 })
 
-#'@rdname successful-methods
-#'@aliases running
+#' @rdname successful-methods
+#' @aliases running
 setMethod(f = "running", signature(.Object = "character", retry = "missing"), definition = function(.Object, retry){
   running(.Object, retry = FALSE)
 })
 
-#'@rdname successful-methods
-#'@aliases running
+#' @rdname successful-methods
+#' @aliases running
 setMethod(f = "running", signature(.Object = "character", retry = "logical"), definition = function(.Object, retry){
   
   process = check(.Object)
@@ -94,29 +93,29 @@ setMethod(f = "running", signature(.Object = "character", retry = "logical"), de
   return(process$statusType == "ProcessStarted" | process$statusType == "ProcessAccepted")
 })
 
-#'@export
+#' @export
 setGeneric(name="error", def=function(.Object, retry){standardGeneric("error")})
 
-#'@rdname successful-methods
-#'@aliases error
+#' @rdname successful-methods
+#' @aliases error
 setMethod(f = "error", signature(.Object = "geojob", retry = "missing"), definition = function(.Object, retry){
   error(id(.Object), retry = FALSE)
 })
 
-#'@rdname successful-methods
-#'@aliases error
-setMethod(f = "error", signature(.Object = "geojob", retry = "missing"), definition = function(.Object, retry){
+#' @rdname successful-methods
+#' @aliases error
+setMethod(f = "error", signature(.Object = "geojob", retry = "logical"), definition = function(.Object, retry){
   error(id(.Object), retry = retry)
 })
 
-#'@rdname successful-methods
-#'@aliases error
+#' @rdname successful-methods
+#' @aliases error
 setMethod(f = "error", signature(.Object = "character", retry = "missing"), definition = function(.Object, retry){
   error(.Object, retry = retry)
 })
 
-#'@rdname successful-methods
-#'@aliases error
+#' @rdname successful-methods
+#' @aliases error
 setMethod(f = "error", signature(.Object = "character", retry = "logical"), definition = function(.Object, retry){
   
   process = check(.Object)
