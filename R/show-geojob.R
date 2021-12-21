821e2f10cbed010538d72e62453b7888ac8c63e1
David Blodgett <dblodgett@usgs.gov>
2021-12-21 09:25:01 -0600

    clean up show files

--- R/show-geojob.R
+++
@@ -1,8 +0,0 @@
-setMethod("show", "geojob", function(object){
-  cat('An object of class "geojob":\n')
-  cat('url:',url(object),'\n')
-  cat('xml:\n', xml(object),'\n')
-  cat(getPackageName(), 'version:', object@package.version,'\n')
-  cat('algorithm version:', object@algorithm.version,'\n')
-  cat('id:', id(object))
-})
