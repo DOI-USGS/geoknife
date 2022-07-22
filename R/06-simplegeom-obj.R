f0af05e628167eca8043fa686099c79d013ed59f
David Blodgett <dblodgett@usgs.gov>
2022-07-21 20:23:03 -0500

    be consistent with pkg.env usage

--- R/06-simplegeom-obj.R
+++ R/06-simplegeom-obj.R
@@ -32,7 +32,8 @@ setClass(
 setMethod("initialize", signature = "simplegeom", 
           definition = function(.Object, ...) {
             .Object@DRAW_NAMESPACE = 'gov.usgs.cida.gdp.draw'
-            .Object@DRAW_SCHEMA = paste0(geoserver_base(), '/www/draw.xsd')
+            .Object@DRAW_SCHEMA = paste0(pkg.env$urls$geoserver_base, 
+                                         '/www/draw.xsd')
             .Object@sf = st_sf(...)
             .Object@sp = as_Spatial(.Object@sf)
             return(.Object)
